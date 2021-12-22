fce49690c25d4a266a92c8059bab105bf192c642
mc-feng <734568955@qq.com>
2021-12-22 11:40:06 +0800

    修改了公司地址

--- src/assets/lang/en.js
+++ src/assets/lang/en.js
@@ -1,106 +1,106 @@
-module.exports = { 
-  menu : { 
-    home:"Home" ,
-    project:"Project Introduction",
-    partner:"Partner",
-    tongxi:"Tongxi health",
-    chanping:"Product experience portal",
-    aboutUs:"About",
-    yijin:"Yijin Health",
-    jieshao:"Products",
-    downLoad:"Download",
-    xinWen:"News Center"
-  }, 
-  home:{
-    yiliao:"Smart medical",
-    baoxian:"Science and Technology Insurance",
-    chuangxin:"Insurance innovation"
+module.exports = {
+  menu: {
+    home: "Home",
+    project: "Project Introduction",
+    partner: "Partner",
+    tongxi: "Tongxi health",
+    chanping: "Product experience portal",
+    aboutUs: "About",
+    yijin: "Yijin Health",
+    jieshao: "Products",
+    downLoad: "Download",
+    xinWen: "News Center"
   },
-  bottom:{
-   gongzong:"Gold Health Public Number",
-   chanping:"Our Products",
-   about:"About Us",
-   chuanzhen:"Fax: (0571) 89915722",
-   youxiang:"Email: tea@tonti.cn",
-   lianxi:"Address:15th Floor, Zhongwei Electronic Building, No. 1819 Xixing Road, Binjiang District, Hangzhou",
-   kefu:"Customer Service:",
-   wangbie:"Network record number:"
+  home: {
+    yiliao: "Smart medical",
+    baoxian: "Science and Technology Insurance",
+    chuangxin: "Insurance innovation"
   },
-  project:{
-    title:"Project Introduction",
-    itemHeader1:"Big data modeling",
-    itemContent1:"Based on the continuity and comprehensive collection of user data, the initial evaluation and early warning of the user's health status is realized.",
-    itemHeader2:"Family health protection",
-    itemContent2:"Assist in the establishment of family health records and improve family health credit.",
-    itemHeader3:"Innovative Science and Technology Insurance",
-    itemContent3:"Utilize big data modeling data and medical scene requirements, combined with smart wearable devices, to provide insurance companies with innovative Science and Technology Insurance advice and landing plans.",
-    itemHeader4:"Lifecycle management",
-    itemContent4:"Through big data modeling, innovation science and technology insurance empowerment, the improvement of family health protection, combined with the use of smart wearable devices, Ultimately manage the Full lifecycle.",
+  bottom: {
+    gongzong: "Gold Health Public Number",
+    chanping: "Our Products",
+    about: "About Us",
+    chuanzhen: "Fax: (0571) 89915722",
+    youxiang: "Email: tea@tonti.cn",
+    lianxi: "Address:20f, building 2, Zicheng international, No. 39, Jincheng Road, Chengxiang street, Xiaoshan District, Hangzhou",
+    kefu: "Customer Service:",
+    wangbie: "Network record number:"
   },
-  partner:{
-    title:"Partner"
+  project: {
+    title: "Project Introduction",
+    itemHeader1: "Big data modeling",
+    itemContent1: "Based on the continuity and comprehensive collection of user data, the initial evaluation and early warning of the user's health status is realized.",
+    itemHeader2: "Family health protection",
+    itemContent2: "Assist in the establishment of family health records and improve family health credit.",
+    itemHeader3: "Innovative Science and Technology Insurance",
+    itemContent3: "Utilize big data modeling data and medical scene requirements, combined with smart wearable devices, to provide insurance companies with innovative Science and Technology Insurance advice and landing plans.",
+    itemHeader4: "Lifecycle management",
+    itemContent4: "Through big data modeling, innovation science and technology insurance empowerment, the improvement of family health protection, combined with the use of smart wearable devices, Ultimately manage the Full lifecycle.",
   },
-  tongxi:{
-    app:"Tongxi Health APP ",
-    introduce:"monitors your temperature in real time",
-    detaile:"We combine insurance products into the sale of flexible wearable devices to solve the worries of high fever medical expenses for children.",
-    saoma:"Scan QR code to download Tong Xi Health App"
+  partner: {
+    title: "Partner"
   },
-  yijin:{
-    name:"Yijin Health APP",
-    introduce:"Indispensable health management tools",
-    detaile:"The official application of Yijin (Hangzhou) Technology Co., Ltd. provides health care for you and your family in all aspects. Real time view of body temperature data and history, escort for health.",
-    saoma:"Scan the QR code and pay attention to the “Yi Jin Health” public number.",
-    sousuo:"App Store Search '义金健康' Download APP（Currently only supports Chinese search）"
+  tongxi: {
+    app: "Tongxi Health APP ",
+    introduce: "monitors your temperature in real time",
+    detaile: "We combine insurance products into the sale of flexible wearable devices to solve the worries of high fever medical expenses for children.",
+    saoma: "Scan QR code to download Tong Xi Health App"
   },
-  aboutUs:{
-    title:"Company Introduction",
-    name:"Yijin (Hangzhou) Health Technology Co., Ltd.",
-    introduce:"Shanghai Tonti Information Technology Co., Ltd. Position: Big Health Technology Insurance Service Provider. Yijin (Hangzhou) Health Technology Co., Ltd., a wholly-owned subsidiary of the company, is a technology operation company with great health data + Internet of Things equipment + innovative insurance + precision medical services. Through the deep integration of medical and health systems and auxiliary intelligent devices and technology insurance products, we use cloud computing to locate and analyze specific scenarios based on the continuous collection of user data, so as to meet the specific needs of users in specific scenarios."
+  yijin: {
+    name: "Yijin Health APP",
+    introduce: "Indispensable health management tools",
+    detaile: "The official application of Yijin (Hangzhou) Technology Co., Ltd. provides health care for you and your family in all aspects. Real time view of body temperature data and history, escort for health.",
+    saoma: "Scan the QR code and pay attention to the “Yi Jin Health” public number.",
+    sousuo: "App Store Search '义金健康' Download APP（Currently only supports Chinese search）"
   },
-  content:{
-    yiliaoTitle:"Wise Information Technology of 120",
-    yiliaoHeader1:"Pocket Hospital",
-    yiliaoContent1:"The main purpose of Pocket Hospital is “patient/user-centered”, which provides users with medical and health information services based on medical procedures. It can provide appointment registration, report inquiry, doctor consultation, inpatient service, and follow-up after hospital. At the same time, it provides self-service functions based on AI such as report interpretation and wisdom medicine. We are committed to creating a friendly, efficient and high-quality medical service environment for customers; strengthening the openness of hospital information, improving the relationship between doctors and patients, and improving customer satisfaction.",
-    yiliaoHeader2:"Perioperative patient service system",
-    yiliaoContent2:"The function of the system covers pre-operative education, surgical tracking and post-operative guidance.The system can deliver content through text/image/video, which helps patients and their families to understand the operation situation in an all-round way, know the risk of surgery, and effectively cooperate with medical staff for health education. For the medical staff, the information system can be used to develop pre-operative psychological assessment, patient feedback, and course learning dynamics for the patient.",
-    yiliaoHeader3:"Interpretation of laboratory test",
-    yiliaoContent3:"The system consists of two parts: one is based on OCR recognition technology to realize the intelligent identification and classification of medical test forms, report forms, and physical examination reports, and quickly establish personal electronic medical records and health files for users. Another part is to provide a description and explanation of the project with the medical data to help users understand their own indicators.",
-    yiliaoHeader4:"Intelligent monitoring system--Multi parameter monitoring",
-    yiliaoContent4:"The newly launched v2.0 version system has added parameters such as HR, RR, oxygen, real-time ECG and so on, while retaining the original basic functions. It can continuously and real-time collect the patient's physical sign data for 24 hours, establish the sign trend record, and prompt the abnormal patients, so as to realize the monitoring of all aspects of vital signs. In the operation interface is more simple and convenient, the navigation bar realizes the one key switch of the interface, the use of equipment, the replacement of beds, etc., which reduces the number of medical staff in daily care and inspection. Improve active care and intervention; non-contact detection to help reduce the risk of cross infection.",
-    baoxianTitle:"InsurTech / Insurance technology",
-    baoxianHeader1:"Health file filing",
-    baoxianContent1:"The health files of insurance company users are often stored in pictures and papers, and then archived and established by means of manual entry. Yijin Health's pioneering health archives system supports the rapid establishment and archiving of test sheets, medical reports, pathology reports, and image reports, helping insurance companies to provide users with more detailed insurance and health services.",
-    baoxianHeader2:"Medical bill filing",
-    baoxianContent2:"In the business, insurance companies involve a large number of medical bill entry and audit work. Based on Yijin Health Intelligent Recognition Technology, we  can realize the rapid identification and archiving of medical bills.",
-    baoxianHeader3:"Intelligent underwriting",
-    baoxianContent3:"By using artificial intelligence technology such as OCR identification, we can realize intelligent identification and interpretation of texts in medical test forms, pathology reports, image reports, and physical examination reports. In combination with the insurance company's underwriting rules, intelligent underwriting audits can be realized, manpower can be liberated, and the quality, efficiency and risk control of underwriting can be improved.",
-    chuangxinTitle:"Insurance innovation",
-    chuangxinHeader1:"Anti-AIDS insurance",
-    chuangxinContent1:"In view of the inadequate protection of AIDS occupational exposure policies, anti-AIDS insurance can provide comprehensive protection for medical personnel, reflecting the hospital's humanistic care.",
-    chuangxinHeader2:"Phage",
-    chuangxinContent2:"It provides insurance coverage for innovative clinical treatments and fills the gap in the insurance market.",
-    chuangxinHeader3:"insurance of medical examination",
-    chuangxinContent3:"For the public hospitals, the medical examination center has a wrong inspection, and the situation of missed inspection provides high insurance coverage, which can alleviate the contradiction between doctors and patients.",
-    chuangxinHeader4:"Internet of Things Insurance",
-    chuangxinContent4:"Focusing on the hospital scene, integrating the Internet of Things resources, providing comprehensive, convenient, efficient and innovative insurance services for both hospitals and patients, helping to build a harmonious relationship between doctors and patients.",
+  aboutUs: {
+    title: "Company Introduction",
+    name: "Yijin (Hangzhou) Health Technology Co., Ltd.",
+    introduce: "Shanghai Tonti Information Technology Co., Ltd. Position: Big Health Technology Insurance Service Provider. Yijin (Hangzhou) Health Technology Co., Ltd., a wholly-owned subsidiary of the company, is a technology operation company with great health data + Internet of Things equipment + innovative insurance + precision medical services. Through the deep integration of medical and health systems and auxiliary intelligent devices and technology insurance products, we use cloud computing to locate and analyze specific scenarios based on the continuous collection of user data, so as to meet the specific needs of users in specific scenarios."
   },
-  mpage:{
-    fuwei:"Our Service",
-    lianxi:"Contact Us",
-    kefu:"Customer Service:",
-    dizhi:"Address:",
-    weizhi:"15th Floor, Zhongwei Electronic Building, No. 1819 Xixing Road, Binjiang District, Hangzhou",
-    guanzhu:"Gold Health Public Number"
+  content: {
+    yiliaoTitle: "Wise Information Technology of 120",
+    yiliaoHeader1: "Pocket Hospital",
+    yiliaoContent1: "The main purpose of Pocket Hospital is “patient/user-centered”, which provides users with medical and health information services based on medical procedures. It can provide appointment registration, report inquiry, doctor consultation, inpatient service, and follow-up after hospital. At the same time, it provides self-service functions based on AI such as report interpretation and wisdom medicine. We are committed to creating a friendly, efficient and high-quality medical service environment for customers; strengthening the openness of hospital information, improving the relationship between doctors and patients, and improving customer satisfaction.",
+    yiliaoHeader2: "Perioperative patient service system",
+    yiliaoContent2: "The function of the system covers pre-operative education, surgical tracking and post-operative guidance.The system can deliver content through text/image/video, which helps patients and their families to understand the operation situation in an all-round way, know the risk of surgery, and effectively cooperate with medical staff for health education. For the medical staff, the information system can be used to develop pre-operative psychological assessment, patient feedback, and course learning dynamics for the patient.",
+    yiliaoHeader3: "Interpretation of laboratory test",
+    yiliaoContent3: "The system consists of two parts: one is based on OCR recognition technology to realize the intelligent identification and classification of medical test forms, report forms, and physical examination reports, and quickly establish personal electronic medical records and health files for users. Another part is to provide a description and explanation of the project with the medical data to help users understand their own indicators.",
+    yiliaoHeader4: "Intelligent monitoring system--Multi parameter monitoring",
+    yiliaoContent4: "The newly launched v2.0 version system has added parameters such as HR, RR, oxygen, real-time ECG and so on, while retaining the original basic functions. It can continuously and real-time collect the patient's physical sign data for 24 hours, establish the sign trend record, and prompt the abnormal patients, so as to realize the monitoring of all aspects of vital signs. In the operation interface is more simple and convenient, the navigation bar realizes the one key switch of the interface, the use of equipment, the replacement of beds, etc., which reduces the number of medical staff in daily care and inspection. Improve active care and intervention; non-contact detection to help reduce the risk of cross infection.",
+    baoxianTitle: "InsurTech / Insurance technology",
+    baoxianHeader1: "Health file filing",
+    baoxianContent1: "The health files of insurance company users are often stored in pictures and papers, and then archived and established by means of manual entry. Yijin Health's pioneering health archives system supports the rapid establishment and archiving of test sheets, medical reports, pathology reports, and image reports, helping insurance companies to provide users with more detailed insurance and health services.",
+    baoxianHeader2: "Medical bill filing",
+    baoxianContent2: "In the business, insurance companies involve a large number of medical bill entry and audit work. Based on Yijin Health Intelligent Recognition Technology, we  can realize the rapid identification and archiving of medical bills.",
+    baoxianHeader3: "Intelligent underwriting",
+    baoxianContent3: "By using artificial intelligence technology such as OCR identification, we can realize intelligent identification and interpretation of texts in medical test forms, pathology reports, image reports, and physical examination reports. In combination with the insurance company's underwriting rules, intelligent underwriting audits can be realized, manpower can be liberated, and the quality, efficiency and risk control of underwriting can be improved.",
+    chuangxinTitle: "Insurance innovation",
+    chuangxinHeader1: "Anti-AIDS insurance",
+    chuangxinContent1: "In view of the inadequate protection of AIDS occupational exposure policies, anti-AIDS insurance can provide comprehensive protection for medical personnel, reflecting the hospital's humanistic care.",
+    chuangxinHeader2: "Phage",
+    chuangxinContent2: "It provides insurance coverage for innovative clinical treatments and fills the gap in the insurance market.",
+    chuangxinHeader3: "insurance of medical examination",
+    chuangxinContent3: "For the public hospitals, the medical examination center has a wrong inspection, and the situation of missed inspection provides high insurance coverage, which can alleviate the contradiction between doctors and patients.",
+    chuangxinHeader4: "Internet of Things Insurance",
+    chuangxinContent4: "Focusing on the hospital scene, integrating the Internet of Things resources, providing comprehensive, convenient, efficient and innovative insurance services for both hospitals and patients, helping to build a harmonious relationship between doctors and patients.",
   },
-  mnav:{
-    home:"Home",
-    about:"About Us",
-    product:"Product",
-    server:"Service",
-    linke:"Contact Us",
-    download1:"Android",
-    download2:"Ios download",
-    guanzhu:"Concerned us"
+  mpage: {
+    fuwei: "Our Service",
+    lianxi: "Contact Us",
+    kefu: "Customer Service:",
+    dizhi: "Address:",
+    weizhi: "20f, building 2, Zicheng international, No. 39, Jincheng Road, Chengxiang street, Xiaoshan District, Hangzhou",
+    guanzhu: "Gold Health Public Number"
+  },
+  mnav: {
+    home: "Home",
+    about: "About Us",
+    product: "Product",
+    server: "Service",
+    linke: "Contact Us",
+    download1: "Android",
+    download2: "Ios download",
+    guanzhu: "Concerned us"
   }
-} 
+}
